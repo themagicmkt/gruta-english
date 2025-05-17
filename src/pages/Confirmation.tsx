1a03ea1344313fec04f3e8ce76d56cf2220a9fa1
themagicmkt <132240308+themagicmkt@users.noreply.github.com>
2025-05-17 13:54:16 -0300

    atualizado checkout

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -282,7 +282,7 @@ const Confirmation = () => {
             </p>
             
             <Button className="w-full bg-[#f4d58d] hover:bg-[#e3c47c] text-[#333333] px-4 py-2 rounded-lg text-lg font-medium shadow-md hover:shadow-lg transition-all" asChild>
-            <a href="https://pay.hotmart.com/N99775963C?off=onbwlmt3" target="_blank" rel="noopener noreferrer">
+            <a href="https://pay.hotmart.com/N99775963C?off=x6e1wxo5" target="_blank" rel="noopener noreferrer">
               Send and Help
             </a>
             </Button>
