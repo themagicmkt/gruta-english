86a7b6501296eacb26fc4cb2159c5c2d7a4cc820
themagicmkt <132240308+themagicmkt@users.noreply.github.com>
2025-06-23 17:30:05 -0300

    rotas atualizadas para upsell

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Confirmation from "./pages/Confirmation";
 import LoadingPage from "./pages/LoadingPage";
-import MonthlyPlan from "./pages/monthlyPlan";
+import MonthlyPlan from "./pages/MonthlyPlan";
+
+
+
 
 const queryClient = new QueryClient();
 
@@ -17,15 +19,16 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/saving" element={<LoadingPage />} />
           <Route path="/confirmation" element={<Confirmation />} />
           <Route path="/Monthly-Plan" element={<MonthlyPlan />} />
+          
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
